6fba07fbb0da46058c276c166926278933cdf04a
hamanaikumi <ikumi.hamana@rakus-partners.co.jp>
2022-06-07 13:59:25 +0900

    firebaseの環境変数を設定

--- src/App.tsx
+++ src/App.tsx
@@ -4,12 +4,21 @@ import { Router } from "./router/Router";
 import firebase from "firebase";
 import { CookiesProvider } from "react-cookie";
 import theme from "./theme/theme";
-
-// firebase関連
-import { firebaseConfig } from "./env";
 import { RecoilRoot } from "recoil";
 
+// firebase関連
+require("dotenv").config();
 require("firebase/firestore");
+
+const firebaseConfig = {
+  apiKey: process.env.REACT_APP_API_KEY,
+  authDomain: process.env.REACT_APP_AUTH_DOMAIN,
+  projectId: process.env.REACT_APP_PROJECT_ID,
+  storageBucket: process.env.REACT_APP_STORAGE_BUCKET,
+  messagingSenderId: process.env.REACT_APP_MESSAGING_SENDER_ID,
+  appId: process.env.REACT_APP_APP_ID,
+  measurementId: process.env.REACT_APP_MEASUREMENT_ID,
+};
 
 if (firebase.apps.length === 0) {
   firebase.initializeApp(firebaseConfig);
